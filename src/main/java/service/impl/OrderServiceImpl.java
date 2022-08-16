a55bc2b82a333e8e7459f2d0e3f0fa1d042da18f
TrinhTung2k <110990730+TrinhTung2k@users.noreply.github.com>
2022-08-16 10:01:08 +0700

    Add files via upload

---
+++ src/main/java/service/impl/OrderServiceImpl.java
@@ -0,0 +1,53 @@
+package service.impl;
+
+import dao.OrderDAO;
+import dao.impl.OrderDAOImpl;
+import model.Address;
+import model.Order;
+import service.OrderService;
+import util.Resources;
+import util.Validator;
+import view.CommonView;
+
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService {
+    private OrderDAO orderDAO;
+    public OrderServiceImpl() {
+        orderDAO = new OrderDAOImpl();
+    }
+    @Override
+    public boolean save(Order order) {
+        if (Validator.getInstance().checkEmpty(order.getName()))
+            return false;
+        else
+            return orderDAO.save(order) > 0;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        Order order = orderDAO.getById(id);
+        if (order != null) {
+            return orderDAO.delete(id) > 0;
+        } else {
+            CommonView.getInstance().displayMessage(Resources.ORDER_NOT_EXIST + id);
+        }
+        return false;
+    }
+
+    @Override
+    public List findAll() {
+        return orderDAO.findAll();
+    }
+
+    @Override
+    public boolean update(int id, Order order) {
+        Order currOrder = orderDAO.getById(id);
+        if (currOrder != null) {
+            return orderDAO.update(id, order) > 0;
+        } else {
+            CommonView.getInstance().displayMessage(Resources.ORDER_NOT_EXIST + id);
+        }
+        return false;
+    }
+}
